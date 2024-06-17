014317567d0422ca49fb5e504029dc275c1a3cda
Andrey Kalugin <jekyl2006@gmail.com>
2024-06-17 09:23:48 +0300

    Switched from Emgu.CV to OpenCV.Sharp

--- src/QuickNV.Onvif/OnvifClient.cs
+++ src/QuickNV.Onvif/OnvifClient.cs
@@ -1,21 +1,12 @@
 ﻿using QuickNV.Onvif.Factorys;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel;
-using System.ServiceModel.Channels;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Xml;
-using System.Xml.Linq;
 
 namespace QuickNV.Onvif
 {
     public class OnvifClient
     {
         public OnvifClientOptions Options { get; private set; }
-        public Uri DeviceServiceAddressUri { get; private set; }
-
         public Device.GetDeviceInformationResponse DeviceInformation { get; private set; }
         public Device.Capabilities Capabilities { get; private set; }
         public Device.DeviceClient DeviceClient { get; private set; }
@@ -35,7 +26,7 @@ namespace QuickNV.Onvif
             return uriBuilder.Uri.ToString();
         }
 
-        private void handleCapabilitiesItem<T>(T t, Func<T, string> getter, Action<T, string> setter)
+        private void HandleCapabilitiesItem<T>(T t, Func<T, string> getter, Action<T, string> setter)
             where T : class, new()
         {
             if (t == null)
@@ -45,7 +36,7 @@ namespace QuickNV.Onvif
                 setter(t, CorrectUri(v));
         }
 
-        private T handleCapabilitiesExtensionItem<T>(T t, string propertyName, Action<T, string> setter)
+        private T HandleCapabilitiesExtensionItem<T>(T t, string propertyName, Action<T, string> setter)
             where T : class, new()
         {
             if (t == null)
@@ -66,7 +57,7 @@ namespace QuickNV.Onvif
                 Options.UserName,
                 Options.Password,
                 Options.ClientCredentialType);
-            UriBuilder deviceClientUriBuilder = new UriBuilder();
+            var deviceClientUriBuilder = new UriBuilder();
             deviceClientUriBuilder.Scheme = Options.Scheme;
             deviceClientUriBuilder.Host = Options.Host;
             deviceClientUriBuilder.Port = Options.Port;
@@ -82,26 +73,24 @@ namespace QuickNV.Onvif
                   Device.CapabilityCategory.All
                 });
                 Capabilities = rep.Capabilities;
-                handleCapabilitiesItem(Capabilities.Analytics, t => t.XAddr, (t, v) => t.XAddr = v);
-                handleCapabilitiesItem(Capabilities.Device, t => t.XAddr, (t, v) => t.XAddr = v);
-                handleCapabilitiesItem(Capabilities.Events, t => t.XAddr, (t, v) => t.XAddr = v);
-                handleCapabilitiesItem(Capabilities.Imaging, t => t.XAddr, (t, v) => t.XAddr = v);
-                handleCapabilitiesItem(Capabilities.Media, t => t.XAddr, (t, v) => t.XAddr = v);
-                handleCapabilitiesItem(Capabilities.PTZ, t => t.XAddr, (t, v) => t.XAddr = v);
+                HandleCapabilitiesItem(Capabilities.Analytics, t => t.XAddr, (t, v) => t.XAddr = v);
+                HandleCapabilitiesItem(Capabilities.Device, t => t.XAddr, (t, v) => t.XAddr = v);
+                HandleCapabilitiesItem(Capabilities.Events, t => t.XAddr, (t, v) => t.XAddr = v);
+                HandleCapabilitiesItem(Capabilities.Imaging, t => t.XAddr, (t, v) => t.XAddr = v);
+                HandleCapabilitiesItem(Capabilities.Media, t => t.XAddr, (t, v) => t.XAddr = v);
+                HandleCapabilitiesItem(Capabilities.PTZ, t => t.XAddr, (t, v) => t.XAddr = v);
 
                 if (Capabilities.Extension.Any != null)
                 {
-                    for (int i = 0; i < Capabilities.Extension.Any.Length; i++)
+                    foreach (var element in Capabilities.Extension.Any)
                     {
-                        var element = Capabilities.Extension.Any[i];
                         if (element == null)
                         {
                             continue;
                         }
                         foreach (var child in element.ChildNodes)
                         {
-                            XmlElement elXAddr = child as XmlElement;
-                            if (elXAddr == null)
+                            if (child is not XmlElement elXAddr)
                                 continue;
                             if (elXAddr.LocalName != "XAddr")
                                 continue;
@@ -112,51 +101,40 @@ namespace QuickNV.Onvif
                 }
 
                 Capabilities.Extension.AnalyticsDevice =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.AnalyticsDevice,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.AnalyticsDevice,
                     nameof(Capabilities.Extension.AnalyticsDevice), (t, v) => t.XAddr = v);
                 Capabilities.Extension.DeviceIO =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.DeviceIO,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.DeviceIO,
                     nameof(Capabilities.Extension.DeviceIO), (t, v) => t.XAddr = v);
                 Capabilities.Extension.DeviceIO =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.DeviceIO,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.DeviceIO,
                     nameof(Capabilities.Extension.DeviceIO), (t, v) => t.XAddr = v);
                 Capabilities.Extension.Display =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.Display,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.Display,
                     nameof(Capabilities.Extension.Display), (t, v) => t.XAddr = v);
                 Capabilities.Extension.Receiver =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.Receiver,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.Receiver,
                     nameof(Capabilities.Extension.Receiver), (t, v) => t.XAddr = v);
                 Capabilities.Extension.Recording =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.Recording,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.Recording,
                     nameof(Capabilities.Extension.Recording), (t, v) => t.XAddr = v);
                 Capabilities.Extension.Replay =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.Replay,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.Replay,
                     nameof(Capabilities.Extension.Replay), (t, v) => t.XAddr = v);
                 Capabilities.Extension.Search =
-                    handleCapabilitiesExtensionItem(Capabilities.Extension.Search,
+                    HandleCapabilitiesExtensionItem(Capabilities.Extension.Search,
                     nameof(Capabilities.Extension.Search), (t, v) => t.XAddr = v);
             }
         }
 
         public string GetExtensionXAddr(string name)
         {
-            if(Capabilities.Extension.Any == null)
-            {
-                return null;
-            }
-            var xmlElement = Capabilities.Extension.Any.FirstOrDefault(t => t.LocalName == name);
-            if (xmlElement == null)
-                return null;
-            foreach (var child in xmlElement.ChildNodes)
-            {
-                XmlElement elXAddr = child as XmlElement;
-                if (elXAddr == null)
-                    continue;
-                if (elXAddr.LocalName != "XAddr")
-                    continue;
-                return elXAddr.InnerText;
-            }
-            return null;
+            var xmlElement = Capabilities.Extension.Any?.FirstOrDefault(t => t.LocalName == name);
+
+            return xmlElement?.ChildNodes.OfType<XmlElement>()
+                .Where(elXAddr => elXAddr.LocalName == "XAddr")
+                .Select(elXAddr => elXAddr.InnerText)
+                .FirstOrDefault();
         }
     }
 }
